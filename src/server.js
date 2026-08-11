6f764768daf5b34efa0e5a6bc7ab12a951fda22e
Hyria Roman <hyrya.roman@gmail.com>
2026-08-11 16:54:03 +0300

    repo: add logging using pino-http

--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import pino from 'pino-http';
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({

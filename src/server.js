e73fb2db2197f737238daae1be67a2884f06bd77
Hyria Roman <hyrya.roman@gmail.com>
2026-08-11 16:52:47 +0300

    server: add cors and json middleware

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
-import express from 'express';
 import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/notes', (req, res) => {
   res.status(200).json({

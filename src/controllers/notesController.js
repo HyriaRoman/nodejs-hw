7d59a06b3ea463aa9ab121ce4040a3aa32eb9957
Hyria Roman <hyrya.roman@gmail.com>
2026-08-19 14:06:01 +0300

    routes: implement deleteNote

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,5 +1,5 @@
-import mongoose from "mongoose";
-import { Note } from "../models/notes.js";
+import mongoose from 'mongoose';
+import { Note } from '../models/notes.js';
 
 export async function getAllNotes(req, res) {
   const notes = await Note.find();
@@ -16,12 +16,13 @@ export async function getNoteById(req, res) {
     noteObjectId = new mongoose.Types.ObjectId(noteId);
   } catch (err) {
     console.error(`Invalid noteId: ${noteId}`, err);
-    return res.status(404).json({ message: `Note "${noteId}" not found`});
+    return res.status(404).json({ message: `Note not found` });
   }
 
   const note = await Note.findById(noteObjectId);
+
   if (!note) {
-    res.status(404).json({ message: `Note "${noteId}" not found`});
+    res.status(404).json({ message: `Note not found` });
   } else {
     res.status(200).json(note);
   }
@@ -33,7 +34,27 @@ export async function createNote(req, res) {
 }
 
 export async function deleteNote(req, res) {
-  res.status(200).json({});
+  const { noteId } = req.params;
+
+  // This is done to catch invalid note ids, and respond with
+  // "404 Not Found" instead of "500 Internal Error"
+  let noteObjectId;
+  try {
+    noteObjectId = new mongoose.Types.ObjectId(noteId);
+  } catch (err) {
+    console.error(`Invalid noteId: ${noteId}`, err);
+    return res.status(404).json({ message: `Note not found` });
+  }
+
+  const note = await Note.findOneAndDelete({
+    _id: noteObjectId,
+  });
+
+  if (!note) {
+    res.status(404).json({ message: `Note not found` });
+  } else {
+    res.status(200).json(note);
+  }
 }
 
 export async function updateNote(req, res) {

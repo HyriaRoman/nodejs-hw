64c455d460912eecfd81838d5e18331a7ced92e3
Hyria Roman <hyrya.roman@gmail.com>
2026-08-19 13:51:55 +0300

    routes: implement getNoteById

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { Note } from "../models/notes.js";
 
 export async function getAllNotes(req, res) {
@@ -6,7 +7,24 @@ export async function getAllNotes(req, res) {
 }
 
 export async function getNoteById(req, res) {
-  res.status(200).json({});
+  const { noteId } = req.params;
+
+  // This is done to catch invalid note ids, and respond with
+  // "404 Not Found" instead of "500 Internal Error"
+  let noteObjectId;
+  try {
+    noteObjectId = new mongoose.Types.ObjectId(noteId);
+  } catch (err) {
+    console.error(`Invalid noteId: ${noteId}`, err);
+    return res.status(404).json({ message: `Note "${noteId}" not found`});
+  }
+
+  const note = await Note.findById(noteObjectId);
+  if (!note) {
+    res.status(404).json({ message: `Note "${noteId}" not found`});
+  } else {
+    res.status(200).json(note);
+  }
 }
 
 export async function createNote(req, res) {

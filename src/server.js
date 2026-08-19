0cbb08965bb019dbc7801705db352607672487e0
Hyria Roman <hyrya.roman@gmail.com>
2026-08-19 12:48:16 +0300

    routes: extract `/notes` route

--- src/server.js
+++ src/server.js
@@ -5,13 +5,8 @@ import cors from 'cors';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/errorHandler.js';
 import notFoundHandler from './middleware/notFoundHandler.js';
-import {
-  createNote,
-  deleteNote,
-  getAllNotes,
-  getNoteById,
-  updateNote,
-} from './controllers/notesController.js';
+
+import notesRouter from './routes/notesRoutes.js';
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
@@ -20,15 +15,7 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 
-app.get('/notes', getAllNotes);
-app.get('/notes/:noteId', getNoteById);
-app.post('/notes', createNote);
-app.patch('notes/:noteId', updateNote);
-app.delete('/notes/:noteId', deleteNote);
-
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+app.use(notesRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);

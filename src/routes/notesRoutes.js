300c8f6b4168c30cd4df7b09e6d9b19fe83126a8
Hyria Roman <hyrya.roman@gmail.com>
2026-08-19 14:39:23 +0300

    routes: insert missing slash

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -13,7 +13,7 @@ const router = Router();
 router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
 router.post('/notes', createNote);
-router.patch('notes/:noteId', updateNote);
+router.patch('/notes/:noteId', updateNote);
 router.delete('/notes/:noteId', deleteNote);
 
 export default router;

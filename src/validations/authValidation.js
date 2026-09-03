b5d8828606eac93416e5b93d266d8ef1cfca7958
Hyria Roman <hyrya.roman@gmail.com>
2026-09-03 19:04:52 +0300

    validation: add auth validation for logging in

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -15,3 +15,19 @@ export const registerUserSchema = {
     }),
   }),
 };
+
+export const loginUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().required().email().messages({
+      'any.required': '`email` is required',
+      'string.base': '`email` must be a string',
+      'string.email': '`email` must be a valid email address',
+    }),
+
+    password: Joi.string().required().min(8).messages({
+      'any.required': '`password` is required',
+      'string.base': '`password` must be a string',
+      'string.min': '`password` should have at least {#limit} characters',
+    }),
+  }),
+};

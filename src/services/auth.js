f1c2d89d48b28de7d7d18e5e9d5274e6851d38b0
Hyria Roman <hyrya.roman@gmail.com>
2026-09-03 18:59:37 +0300

    services: fix auth service imports

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,5 @@
-import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/time';
-import { Session } from '../models/session';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/time.js';
+import { Session } from '../models/session.js';
 
 export function createSession(userId) {
   const accessToken = crypto.randomUUID();

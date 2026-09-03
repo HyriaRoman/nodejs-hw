d7dcd7d8e22e15008a9c2ab7a1d4d208b65895a3
Hyria Roman <hyrya.roman@gmail.com>
2026-09-03 19:27:53 +0300

    routes: add logout route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 
-import { loginUser, refreshUserSession, registerUser } from '../controllers/authController.js';
+import { loginUser, logoutUser, refreshUserSession, registerUser } from '../controllers/authController.js';
 import { loginUserSchema, registerUserSchema } from '../validations/authValidation.js';
 
 const router = Router();
@@ -9,5 +9,6 @@ const router = Router();
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/refresh', refreshUserSession);
+router.post('/auth/logout', logoutUser);
 
 export default router;

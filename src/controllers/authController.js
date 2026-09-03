dc13f7fd01b44b9e62b4c6cd9f269efca8353efd
Hyria Roman <hyrya.roman@gmail.com>
2026-09-03 19:11:14 +0300

    controllers: add session refreshing

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,7 +2,7 @@ import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 
 import { User } from '../models/user.js';
-import { Session } from "../models/session.js";
+import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 
 export async function registerUser(req, res) {
@@ -27,7 +27,6 @@ export async function registerUser(req, res) {
   res.status(201).json(newUser);
 }
 
-
 export async function loginUser(req, res) {
   const { email, password } = req.body;
 
@@ -48,4 +47,41 @@ export async function loginUser(req, res) {
   setSessionCookies(res, newSession);
 
   res.status(201).json(user);
+}
+
+export async function refreshUserSession(req, res) {
+  const { sessionId, refreshToken } = req.cookies;
+
+  if (!sessionId || !refreshToken) {
+    throw createHttpError(401, 'Missing session credentials');
+  }
+
+  const session = await Session.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  const isSessionTokenExpired = session.refreshTokenValidUntil < new Date();
+
+  if (isSessionTokenExpired) {
+    await session.deleteOne();
+    res.clearCookie('sessionId');
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+    throw createHttpError(401, 'Session token expired');
+  }
+
+  await session.deleteOne();
+
+  const newSession = await createSession(session.userId);
+
+  setSessionCookies(res, newSession);
+
+  res.status(200).json({
+    message: 'Session refreshed',
+  });
 }

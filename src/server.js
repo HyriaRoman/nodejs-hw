26ffc56a0ff2befbdf0deaa235da9a76c29e5330
Hyria Roman <hyrya.roman@gmail.com>
2026-09-03 19:00:31 +0300

    routes: add auth/register route

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 
@@ -8,6 +9,7 @@ import { logger } from './middleware/logger.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 
+import authRouter from './routes/authRoutes.js';
 import notesRouter from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
 
@@ -19,7 +21,9 @@ await connectMongoDB();
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
+app.use(authRouter);
 app.use(notesRouter);
 
 app.use(notFoundHandler);

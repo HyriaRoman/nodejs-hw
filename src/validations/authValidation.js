8d9d2b18069eae4a50ba5bb0a3ccadf1248c33ad
Hyria Roman <hyrya.roman@gmail.com>
2026-09-11 13:01:59 +0300

    validation: add reset password validation

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -34,10 +34,24 @@ export const loginUserSchema = {
 
 export const requestResetEmailSchema = {
   [Segments.BODY]: Joi.object({
-    email: Joi.string().email().required().messages({
+    email: Joi.string().required().email().messages({
       'any.required': '`email` is required',
       'string.base': '`email` must be a string',
       'string.email': '`email` must be a valid email address',
+    }),
+  }),
+};
+
+export const resetPasswordSchema = {
+  [Segments.BODY]: Joi.object({
+    token: Joi.string().required().messages({
+      'any.required': '`token` is required',
+      'string.base': '`token` must be a string',
+    }),
+    password: Joi.string().required().min(8).messages({
+      'any.required': '`password` is required',
+      'string.base': '`password` must be a string',
+      'string.min': '`password` should have at least {#limit} characters',
     }),
   }),
 };

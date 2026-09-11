10cc254d99b6576eb54178335a167be3153f4cf9
Hyria Roman <hyrya.roman@gmail.com>
2026-09-11 12:05:01 +0300

    routes: add request password reset route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,20 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+import handlebars from 'handlebars';
 
 import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
+import jsonwebtoken from 'jsonwebtoken';
+import { sendEmail } from '../utils/sendEmail.js';
+
+// We do this here so we don't have to re-read the file on every request
+const emailTemplatePath = path.resolve('src/templates/reset-password-email.html');
+const emailTemplateSource = await fs.readFile(emailTemplatePath, 'utf-8');
+const emailTemplate = handlebars.compile(emailTemplateSource);
 
 export async function registerUser(req, res) {
   const { email, password } = req.body;
@@ -98,4 +109,46 @@ export async function logoutUser(req, res) {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
+}
+
+export async function requestResetEmail(req, res) {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(200).json({
+      message: 'Password reset email sent successfully',
+    });
+  }
+
+  const resetToken = jsonwebtoken.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+  const html = emailTemplate({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'NoteHub: Reset your password',
+      html,
+    });
+  } catch {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
+
+  // Та сама "нейтральна" відповідь
+  res.status(200).json({
+    message: 'Password reset email sent successfully',
+  });
 }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,18 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 
-import { loginUser, logoutUser, refreshUserSession, registerUser } from '../controllers/authController.js';
-import { loginUserSchema, registerUserSchema } from '../validations/authValidation.js';
+import {
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+  registerUser,
+  requestResetEmail,
+} from '../controllers/authController.js';
+import {
+  loginUserSchema,
+  registerUserSchema,
+  requestResetEmailSchema,
+} from '../validations/authValidation.js';
 
 const router = Router();
 
@@ -10,5 +20,10 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/refresh', refreshUserSession);
 router.post('/auth/logout', logoutUser);
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
 
 export default router;
